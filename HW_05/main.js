7a468b774b2e9debd2d37fa17191eca032f7ff13
andrii vorflik <andrii@gmail.com>
2024-02-26 13:27:54 +0200

    added hw_05

---
+++ HW_05/main.js
@@ -0,0 +1,32 @@
+/* let a = 2;
+let b = 3;
+
+let sum = a + b;
+a = 2;
+let mul = a * b;
+
+
+const greeting = "Hello"; 
+const superHero = 'Peter Parker';
+
+console.log(sum);
+console.log(mul);
+console.log(greeting + ' ' + superHero); */
+
+/* const userName = prompt('Як вас звуть?');
+const product = prompt('Введіть назву товару');
+const postOfficeNumber = prompt('Введіть номер відділення нової пошти');
+
+console.log(
+  `${userName}, ви придбали у нашому магазині ${product}. Товар буде доставлено на відділення нової пошти №${postOfficeNumber}.`
+); */
+
+
+let num1 = +prompt('Введіть перше число:');
+let num2 = +prompt('Введіть друге число:');
+
+console.log(typeof num1);
+console.log(typeof num2);
+console.log(num1 + num2);
+
+
